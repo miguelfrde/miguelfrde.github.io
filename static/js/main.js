eb461468e06273d3b30e276c87343edda8a9f51a
Miguel Flores Ruiz de Eguino <miguel.frde@gmail.com>
2015-11-30 10:37:32 -0600

    Add visited/lived countries map

--- static/js/main.js
+++ static/js/main.js
@@ -26,4 +26,33 @@ $(document).ready(function() {
   $('img.lazy').lazyload();
 
   $('#year').text(new Date().getFullYear());
+
+  $.get('/static/data/countries.json', function (countries) {
+    var visited = countries['visited'];
+    var data = {};
+    ['lived', 'visited'].forEach(function (category) {
+      countries[category].forEach(function (obj) {
+        data[obj['code']] = { fillKey: category };
+      });
+    });
+
+    var map = new Datamap({
+      responsive: true,
+      element: document.getElementById('world-map'),
+      fills: {
+        visited: '#d97b29',
+        lived: '#ca5c1c',
+        defaultFill: '#c5d3d9'
+      },
+      geographyConfig: {
+        highlightOnHover: false,
+        popupOnHover: false
+      },
+      data: data
+    });
+
+    $(window).on('resize', function() {
+      map.resize();
+    });
+  });
 });
